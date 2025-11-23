998a9cc889dc4a148b64b3ba8cd06e26df00dc28
Мамизеров Никита <nmamizerov@gmail.com>
2025-11-23 17:09:12 +0300

    test

--- places/views.py
+++ places/views.py
@@ -2,7 +2,10 @@ import logging
 from simulators.models import SimulatorUser, Simulator
 from django.shortcuts import render, get_object_or_404
 from rest_framework import permissions
-from backend.application_viewset import AdminApplicationViewSet, ApplicationReadOnlyViewSet
+from backend.application_viewset import (
+    AdminApplicationViewSet,
+    ApplicationReadOnlyViewSet,
+)
 from .serializers import PlaceSerializer, PlaceUserInfoSerializer
 from .models import Place, PlaceUser, PlaceExpertException
 from pages.models import UserPageProgress
@@ -13,6 +16,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from notifications.models import Notification
 from bot.models import tgUser, tgSend
+
 # logging.basicConfig(filename='example.log', level=logging.DEBUG)
 import re
 
@@ -28,10 +32,10 @@ class AdminPlaceViewSet(AdminApplicationViewSet):
 
     def get_queryset(self):
         queryset = []
-        if self.params.get('page') and not self.action in SAFE_ACTIONS:
-            queryset = Place.objects.filter(page__id=self.params.get('page'))
-        elif self.params.get('lesson') and not self.action in SAFE_ACTIONS:
-            queryset = Place.objects.filter(page__lesson__id=self.params.get('lesson'))
+        if self.params.get("page") and not self.action in SAFE_ACTIONS:
+            queryset = Place.objects.filter(page__id=self.params.get("page"))
+        elif self.params.get("lesson") and not self.action in SAFE_ACTIONS:
+            queryset = Place.objects.filter(page__lesson__id=self.params.get("lesson"))
         else:
             queryset = Place.objects.all()
         return queryset
@@ -43,40 +47,50 @@ class PlaceViewSet(ApplicationReadOnlyViewSet):
     serializer_class = PlaceSerializer
     queryset = Place.objects.all()
 
-    @action(detail=False, methods=['post'])
+    @action(detail=False, methods=["post"])
     def comment(self, request, *args, **kwargs):
-        if not 'place' in request.data or not 'comment' in request.data or not 'answer_id' in request.data:
+        if (
+            not "place" in request.data
+            or not "comment" in request.data
+            or not "answer_id" in request.data
+        ):
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        place = Place.objects.get(id=request.data['place'])
+        place = Place.objects.get(id=request.data["place"])
         p_user = PlaceUser.objects.filter(user=request.user, place=place).first()
         page_user = UserPageProgress.objects.get(page=place.page, user=request.user)
         page_user_place_idx = 0
         if not p_user:
             return Response(status=status.HTTP_404_NOT_FOUND)
         for idx, commented_answer in enumerate(p_user.commented_answers):
-            if commented_answer['id'] == request.data['answer_id']:
-                p_user.commented_answers[idx]['comment'] = request.data['comment']
-                p_user.commented_answers[idx]['commented'] = True
+            if commented_answer["id"] == request.data["answer_id"]:
+                p_user.commented_answers[idx]["comment"] = request.data["comment"]
+                p_user.commented_answers[idx]["commented"] = True
         for idx_p, p_place in enumerate(page_user.places):
-            if p_place['id'] == place.id:
-                for idx_cp, commented_answer in enumerate(p_place['commented_answers']):
-                    if commented_answer['id'] == request.data['answer_id']:
+            if p_place["id"] == place.id:
+                for idx_cp, commented_answer in enumerate(p_place["commented_answers"]):
+                    if commented_answer["id"] == request.data["answer_id"]:
                         page_user_place_idx = idx_p
-                        page_user.places[idx_p]['commented_answers'][idx_cp]['comment'] = request.data['comment']
-                        page_user.places[idx_p]['commented_answers'][idx_cp]['commented'] = True
-        commented_place = PlaceUser.objects.filter(id=request.data['answer_id']).first()
+                        page_user.places[idx_p]["commented_answers"][idx_cp][
+                            "comment"
+                        ] = request.data["comment"]
+                        page_user.places[idx_p]["commented_answers"][idx_cp][
+                            "commented"
+                        ] = True
+        commented_place = PlaceUser.objects.filter(id=request.data["answer_id"]).first()
 
-        notification = Notification(from_user=request.user,
-                                    to_user=commented_place.user,
-                                    answer=commented_place,
-                                    text=request.data['comment'],
-                                    type='user_comment')
+        notification = Notification(
+            from_user=request.user,
+            to_user=commented_place.user,
+            answer=commented_place,
+            text=request.data["comment"],
+            type="user_comment",
+        )
         notification.save()
         tg_user = tgUser.objects.filter(user=commented_place.user).first()
-        if tg_user is not None and tg_user.active: 
-            TAG_RE = re.compile(r'<[^>]+>')
-            question_text = TAG_RE.sub('', commented_place.place.text)
-            
+        if tg_user is not None and tg_user.active:
+            TAG_RE = re.compile(r"<[^>]+>")
+            question_text = TAG_RE.sub("", commented_place.place.text)
+
             message = f"""Вы получили комментарий на ваш ответ к вопросу: 
 <b>{question_text}</b>
 
@@ -84,26 +98,25 @@ class PlaceViewSet(ApplicationReadOnlyViewSet):
 <i>{request.data['comment']}</i>
 
 <a href=\"https://skillslab.center/blogs/{commented_place.place.page.lesson.slug}\">Перейти к уроку</a>"""
-            tgSend.objects.create(
-                tg=tg_user,
-                message=message
-            )
+            tgSend.objects.create(tg=tg_user, message=message)
 
         if place.comment_number > len(p_user.commented_answers):
             answer_to_comment = place.get_answer_to_comment(request.user)
             if not answer_to_comment:
-                page_user.places[page_user_place_idx]['complete'] = True
+                page_user.places[page_user_place_idx]["complete"] = True
                 p_user.is_completed = True
-                request.data['user_answer'] = p_user.answers
+                request.data["user_answer"] = p_user.answers
                 page_user.save()
                 p_user.save()
                 return self.complete_place(place, request)
             p_user.commented_answers.append(answer_to_comment)
-            page_user.places[page_user_place_idx]['commented_answers'].append(answer_to_comment)
+            page_user.places[page_user_place_idx]["commented_answers"].append(
+                answer_to_comment
+            )
         else:
-            page_user.places[page_user_place_idx]['complete'] = True
+            page_user.places[page_user_place_idx]["complete"] = True
             p_user.is_completed = True
-            request.data['user_answer'] = p_user.answers
+            request.data["user_answer"] = p_user.answers
             page_user.save()
             p_user.save()
             return self.complete_place(place, request)
@@ -118,26 +131,62 @@ class PlaceViewSet(ApplicationReadOnlyViewSet):
 
     def complete_place(self, place, request):
         simulator = place.page.lesson.simulator
-        page_user = UserPageProgress.objects.get(page=place.page, user=request.user)
+        page_user = UserPageProgress.objects.filter(
+            page=place.page, user=request.user
+        ).first()
 
         try:
             place_action = getattr(Place, f"{place.type}_action")
         except AttributeError:
-            raise NotImplementedError("Class `{}` does not implement `{}`".format(Place.__class__.__name__, place.type))
+            raise NotImplementedError(
+                "Class `{}` does not implement `{}`".format(
+                    Place.__class__.__name__, place.type
+                )
+            )
 
         try:
-            if place.type in ('question', 'questionuserchoice'):
-                place_action(place, request.user, page_user, answers=request.data['answers'])
-            elif place.type in ('openquestion', 'openquestionexpert', 'questionanswercheck'):
-                place_action(place, request.user, page_user, user_answer=request.data['user_answer'])
-            elif place.type in ('aiopenquestion', 'testaiopenquestion'):
-                place_action(place, request.user, page_user, user_answer=request.data['user_answer'])
-            elif place.type == 'questionrange':
-                place_action(place, request.user, page_user, user_answer_min=request.data['user_answer_min'], user_answer_max=request.data['user_answer_max'])
+            if place.type in ("question", "questionuserchoice"):
+                place_action(
+                    place, request.user, page_user, answers=request.data["answers"]
+                )
+            elif place.type in (
+                "openquestion",
+                "openquestionexpert",
+                "questionanswercheck",
+            ):
+                place_action(
+                    place,
+                    request.user,
+                    page_user,
+                    user_answer=request.data["user_answer"],
+                )
+            elif place.type in ("aiopenquestion", "testaiopenquestion"):
+                place_action(
+                    place,
+                    request.user,
+                    page_user,
+                    user_answer=request.data["user_answer"],
+                )
+            elif place.type == "questionrange":
+                place_action(
+                    place,
+                    request.user,
+                    page_user,
+                    user_answer_min=request.data["user_answer_min"],
+                    user_answer_max=request.data["user_answer_max"],
+                )
             else:
                 place_action(place, request.user, page_user)
         except PlaceExpertException:
             return Response(status=status.HTTP_204_NO_CONTENT)
 
-        next_place_data, next_page_data, next_lesson_data = place.finish_complete(request.user, simulator, page_user)
-        return Response({"place": next_place_data, "page": next_page_data, "lesson": next_lesson_data})
+        next_place_data, next_page_data, next_lesson_data = place.finish_complete(
+            request.user, simulator, page_user
+        )
+        return Response(
+            {
+                "place": next_place_data,
+                "page": next_page_data,
+                "lesson": next_lesson_data,
+            }
+        )
